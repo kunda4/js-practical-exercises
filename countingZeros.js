cd9e98dc560e5cd636dcb34a6c329831df7d2e02
Agnes IRADUKUNDA <101483232+kunda4@users.noreply.github.com>
2023-07-19 11:46:24 +0200

    Javascript exercices

---
+++ countingZeros.js
@@ -0,0 +1,22 @@
+//Count Total number of zeros from 1 up to n?
+
+function countZeros(n) {
+  let count = 0;
+  let arr = [];
+
+  for (let i = 1; i <= n; i++) {
+    // if (i % 10 == 0) {
+    //   count++;
+    arr.push(i);
+  }
+  console.log(arr);
+
+  const result = arr
+    .toString()
+    .split("")
+    .filter((arr) => arr == 0).length;
+
+  return result;
+}
+
+console.log(countZeros(1000));
