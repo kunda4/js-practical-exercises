a5859b7bcc06bd9aa39eb0d23b6b3865bd71036d
Agnes Iradukunda <kundaaggy@gmail.com>
2024-02-09 11:50:46 +0200

    add forEach method

--- JS/forEachandmap.js
+++ JS/forEachandmap.js
@@ -1,6 +1,14 @@
 // map
+// return new array 
 
 const numbers = [2,4,6,7,8]
 
 const myNum = numbers.map(item => item*2)
 console.log(myNum)
+
+//forEach
+// forEach return undefined
+const numberz= [2,4,6,7,8]
+
+const myNumbers = numberz.forEach(item => item*2)
+console.log(myNumbers)
