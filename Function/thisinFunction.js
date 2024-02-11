14ee3faf10bd500c69e9b5237c12f15c2b32b1bd
Agnes Iradukunda <kundaaggy@gmail.com>
2024-02-11 05:56:42 +0200

    add arrowMethod

--- Function/thisinFunction.js
+++ Function/thisinFunction.js
@@ -8,7 +8,13 @@ class myClass{
             console.log(this.value)
         }, 1000)
     }
+    arrowMethod(){
+        setTimeout(() => {
+            console.log(this.value)
+        }, 1000)
+    }
 }
 
 const instance = new myClass()
 instance.normalMethod()
+instance.arrowMethod()
