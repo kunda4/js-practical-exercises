dd95085f5e91e2a461144a49b1ec640dabd5025d
Agnes Iradukunda <kundaaggy@gmail.com>
2024-02-17 00:30:36 +0200

    flattern array with reduce

--- Challenge/flatternArray.js
+++ Challenge/flatternArray.js
@@ -1,7 +1,12 @@
 // Flattern an array of arrays
-
+// with using the flat() method
 let arr = [[0,1], [2,3], [4,5]]
 
 let myNewArr = arr.flat()
-
 console.log(myNewArr)
+
+// without using the flat() method
+
+let newArr = arr.reduce((prev, curr)=> prev.concat(curr), [])
+
+console.log(newArr)
