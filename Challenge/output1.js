fd2ae8e67c501ece1e191bf3c5cfce76c83572cd
Agnes Iradukunda <kundaaggy@gmail.com>
2024-02-17 00:21:17 +0200

    the output

---
+++ Challenge/output1.js
@@ -0,0 +1,12 @@
+//What will be the output of the code and provide the reason
+
+var x = Math.floor(Math.random())
+
+if(x>0.5){
+    var x=1
+} else{
+    var x=2
+}
+console.log(x)
+
+// the output will be 2
