3e147bc1a80a6f5cbca25dc4848c6fdd6ab81204
Agnes Iradukunda <kundaaggy@gmail.com>
2024-02-18 23:11:13 +0200

    add index.js

---
+++ Challenge/randomChar/index.js
@@ -0,0 +1,13 @@
+function generateRandom(){
+    const chars = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
+
+    const strLength = 7;
+    let result = ''
+
+    for(let i = 0; i<strLength; i++){
+        const random = Math.floor(Math.random * chars.length)
+        result += chars.substring(random, random+1)
+    }
+
+    document.getElementById('randomChar').innerHTML = result
+}
