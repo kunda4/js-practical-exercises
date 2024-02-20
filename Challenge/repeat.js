a83e41609caa9546cb0221cdaea4ed4c8bc7fe9d
Agnes Iradukunda <kundaaggy@gmail.com>
2024-02-20 21:25:18 +0200

    repeat method

---
+++ Challenge/repeat.js
@@ -0,0 +1,9 @@
+// Guesse the output in the following codes
+
+let str = '123';
+let result1 = str.repeat(0);
+let result2 = str.repeat(3.5);
+
+console.log((result1 + result2).length)
+
+// the output is 9
