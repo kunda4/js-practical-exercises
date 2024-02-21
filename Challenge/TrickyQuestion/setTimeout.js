4cd9e45b6fbcf7b1553b78c9199ba30f2d7d4d58
Agnes Iradukunda <kundaaggy@gmail.com>
2024-02-21 16:25:16 +0200

    working on setTimeout

---
+++ Challenge/TrickyQuestion/setTimeout.js
@@ -0,0 +1,10 @@
+// Guesse the output of the following block of codes
+
+setTimeout(() => {console.log('string1');},'1000')
+setTimeout(() => {console.log('string2');},'1 second')
+setTimeout(() => {console.log('Number');},2000)
+
+// the output will be 
+// string2
+// string1
+// Number
