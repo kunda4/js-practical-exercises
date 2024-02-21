d69968123d7dacc4432c837f3369edb47fdaafd4
Agnes Iradukunda <kundaaggy@gmail.com>
2024-02-21 12:30:47 +0200

    bitwise shift left

---
+++ Challenge/TrickyQuestion/bitwise.js
@@ -0,0 +1,14 @@
+//JavaScript coding output
+
+const a = 5;
+const b = 2;
+
+// number is being converted to binary first
+
+console.log(a.toString(2)) // 101 and add 2 zeros from b => 10100
+console.log(typeof(a<<b)) // 10100 and then put in integer
+
+console.log(parseInt(a<<b)) // this will be 20
+
+// and the output of the typeOf(a<<b) will be number
+
