4c7038afe3e16740df93f71b102e15188872c6d0
Agnes Iradukunda <kundaaggy@gmail.com>
2024-02-21 16:05:31 +0200

    add scope2

---
+++ Challenge/TrickyQuestion/scope2.js
@@ -0,0 +1,17 @@
+// What the output of the following codes
+
+var a = 5;
+
+const x = (a) => 
+console.log(a);
+console.log(a);
+
+const y = function (b){console.log(b)};
+
+x(1);
+y(3);
+
+// the output of the following code will be
+// 5
+// 1
+// 3
