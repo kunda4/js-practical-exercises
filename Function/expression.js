b588050287345d412f7e08a320a0d917f094816b
Agnes Iradukunda <kundaaggy@gmail.com>
2024-02-22 10:48:05 +0200

    function expression

---
+++ Function/expression.js
@@ -0,0 +1,14 @@
+// Anonynous Function Expression
+
+const add = function(a,b){
+    return a + b
+}
+
+console.log(add(3,5))
+
+// named Function Expression
+
+const mult = function multiplication(a,b){
+    return a * b
+}
+console.log(mult(3,5))
