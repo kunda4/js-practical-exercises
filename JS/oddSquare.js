2d488ef74b09dc570cfffb2f886bf3ddbe3e7e2f
Agnes Iradukunda <kundaaggy@gmail.com>
2024-04-08 01:20:16 +0200

    add the oddSquare function

---
+++ JS/oddSquare.js
@@ -0,0 +1,14 @@
+/**
+Odd Squares
+
+You are given an array of numbers. Your task is to implement a function called oddSquares that filters out the even numbers from the array and then maps the remaining numbers to their squares.
+
+Example:
+oddSquares([1, 2, 3, 4, 5, 6]); // should return [1, 9, 25]
+oddSquares([10, 15, 20, 25, 30]); // should return [225, 625]
+
+ */
+
+const oddSquares = (arr) => arr.filter(ele => ele%2 !== 0).map(ele => ele*ele);
+
+console.log(oddSquares([1,2,3,4,5,6,7,8]));
